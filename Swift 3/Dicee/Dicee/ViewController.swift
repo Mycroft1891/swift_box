8d2ccf13888e6e3bcf0cb841ec676d2b2c642c7d
Mycroft1891 <mycroft1891@gmail.com>
2017-05-16 03:59:35 -0500

    Finish up Dicee App

--- Swift 3/Dicee/Dicee/ViewController.swift
+++ Swift 3/Dicee/Dicee/ViewController.swift
@@ -1,13 +1,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var random1: Int = 0
+    var random2: Int = 0
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setRandomDiceFaces()
     }
 
     override func didReceiveMemoryWarning() {
@@ -15,8 +20,21 @@ class ViewController: UIViewController {
     }
 
     
-    @IBAction func rollButtonPressed(_ sender: Any) {
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        setRandomDiceFaces()
     }
-
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        setRandomDiceFaces()
+    }
+    
+    // generate random dice faces
+    func setRandomDiceFaces() {
+        random1 = Int(arc4random_uniform(6))
+        random2 = Int(arc4random_uniform(6))
+        
+        dice1.image = UIImage(named: diceArray[random1])
+        dice2.image = UIImage(named: diceArray[random2])
+    }
 }
 
