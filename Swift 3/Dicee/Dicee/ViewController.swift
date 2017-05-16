5babd9f0b2d57208c9ceb96ab18af2372a6f7613
Mycroft1891 <mycroft1891@gmail.com>
2017-05-16 01:07:53 -0500

    Add new Dicee project

---
+++ Swift 3/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+    }
+
+}
+
---
+++ Swift 3/Playgrounds/MakeSchool.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var greetings = ["Hello", "Hallo", "Hola", "こんにちは"]
+
+for greeting in greetings {
+    print(greeting)
+}
+
+
