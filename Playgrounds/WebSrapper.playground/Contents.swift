4ef98d019bbe019a3625a6a8ae701a0e072fcd96
Mycroft1891 <mycroft1891@gmail.com>
2017-06-09 16:54:05 -0500

    Add Actions and Sound to Test Game

--- Playgrounds/WebSrapper.playground/Contents.swift
+++ Playgrounds/WebSrapper.playground/Contents.swift
@@ -3,21 +3,21 @@
 import UIKit
 import PlaygroundSupport
 
-PlaygroundPage.current.needsIndefiniteExecution = true
-
-let url = URL(string: "http://mangafox.me/manga/onepunch_man/")!
-
-let task = URLSession.shared.dataTask(with: url) { data, response, error in
-    guard let data = data, error == nil else {
-        print(error!)
-        return
-    }
-    
-    let string = String(data: data, encoding: .utf8)
-    print(string!)
-}
-
-task.resume()
+//PlaygroundPage.current.needsIndefiniteExecution = true
+//
+//let url = URL(string: "http://mangafox.me/manga/onepunch_man/")!
+//
+//let task = URLSession.shared.dataTask(with: url) { data, response, error in
+//    guard let data = data, error == nil else {
+//        print(error!)
+//        return
+//    }
+//    
+//    let string = String(data: data, encoding: .utf8)
+//    print(string!)
+//}
+//
+//task.resume()
 
 
 //var bits: UInt32 = 0x1
@@ -25,3 +25,11 @@ task.resume()
 //bits << 2
 //bits << 3
 //bits << 4
+
+
+let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//print(documentPath)
+
+let notesPath = documentPath.appendingPathComponent("Note")
+
+print(notesPath.path)
--- Swift 3/Test Game/Test Game/GameScene.swift
+++ Swift 3/Test Game/Test Game/GameScene.swift
@@ -9,16 +9,34 @@
 import SpriteKit
 import GameplayKit
 
+let wallMask : UInt32 = 0x1 << 0  // 1
+let ballMask : UInt32 = 0x1 << 1  // 2
+let sqrMask  : UInt32 = 0x1 << 2  // 4
+let pegMask  : UInt32 = 0x1 << 3  // 8
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var cannon : SKSpriteNode!
+    var square : SKSpriteNode!
     var touchLocation : CGPoint = CGPoint.zero
 //    var ball : SKSpriteNode!
     
     override func didMove(to view: SKView) {
         cannon = self.childNode(withName: "cannon") as! SKSpriteNode
+        square = self.childNode(withName: "sqaure") as! SKSpriteNode
 //        ball = self.childNode(withName: "ball") as! SKSpriteNode
         self.physicsWorld.contactDelegate = self
+        
+        let degress = 90.0
+        let rad = degress * .pi / 180.0
+        let action = SKAction.rotate(byAngle: CGFloat(rad), duration: 1)
+        square.run(SKAction.repeatForever(action))
+        
+        let backgroundSound = SKAudioNode(fileNamed: "bg.mp3")
+        self.addChild(backgroundSound)
+        
+        backgroundSound.run(SKAction.play())
+
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -55,11 +73,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(ball)
         ball.position = cannon.position
         ball.zPosition = 0
+        
         let angleRadians = Float(cannon.zRotation)
         let speed = CGFloat(75.0)
         let speedX = CGFloat(cosf(angleRadians)) * speed
         let speedY = CGFloat(sinf(angleRadians)) * speed
+        
         ball.physicsBody?.applyImpulse(CGVector(dx: speedX, dy: speedY))
+        ball.physicsBody?.collisionBitMask = wallMask | ballMask | pegMask
+        ball.physicsBody?.contactTestBitMask = ball.physicsBody!.collisionBitMask | sqrMask
+        
+        self.run(SKAction.playSoundFileNamed("shoot.wav", waitForCompletion: true))
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -76,6 +100,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        print("Contact")
+        let ball  = (contact.bodyA.categoryBitMask == ballMask) ? contact.bodyA : contact.bodyB
+        let other = (ball == contact.bodyA) ? contact.bodyB : contact.bodyA
+        
+        switch other.categoryBitMask {
+        case wallMask:
+            print("We hit a wall")
+        case sqrMask:
+            print("We hit a square")
+        case pegMask:
+            print("We hit a peg")
+        case ballMask:
+            hitBall(ball: other)
+        default:
+            break
+        }
+
+    }
+    
+    func hitBall(ball: SKPhysicsBody) {
+        print("We hit a ball")
+        let spark = SKEmitterNode(fileNamed: "MyParticle")!
+        spark.position = ball.node!.position
+        self.addChild(spark)
+        ball.node?.removeFromParent()
+        
+        self.run(SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: true))
     }
 }
