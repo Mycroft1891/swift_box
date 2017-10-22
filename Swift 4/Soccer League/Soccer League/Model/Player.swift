d7ffd8348ac148af43e4da4741a753ce9e131fff
Mycroft1891 <mycroft1891@gmail.com>
2017-10-22 21:55:32 +0200

    Add seagues and choice handling

---
+++ Swift 4/Soccer League/Soccer League/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Soccer League
+//
+//  Created by Alexander on 10/22/17.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var skillLevel: String?
+    var league: String?
+}
